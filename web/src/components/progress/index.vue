8d61cdd4b8098ad87afcf33221feae4d11073bed
jjm2473 <jjm2473@gmail.com>
2026-07-07 19:55:08 +0800

    web: partitions progress bar color aligns with disk

--- web/src/components/progress/index.vue
+++ web/src/components/progress/index.vue
@@ -21,19 +21,10 @@ const props = defineProps({
     }
 })
 const backgroundColor = computed(() => {
-    if (props.value >= 80) {
-        return "#e45e5e"
-    }
-    if (props.value >= 70) {
-        return "#ff9800"
-    }
-    if (props.value >= 60) {
-        return "#297ff3"
-    }
-    if (props.value > 0) {
-        return "#53c31b"
-    }
-    return ""
+    if (props.value < 60) return "#2fc867"; // Green
+    if (props.value < 75) return "#fdd835"; // Yellow
+    if (props.value < 90) return "#f97316"; // Orange
+    return "#dc2626"; // Red
 })
 </script>
 <style lang="scss" scoped>
